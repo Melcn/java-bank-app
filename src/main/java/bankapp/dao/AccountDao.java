e0c23a18404165b8e52fa478432592b8af0ea45e
Melcn <melaniearinosy@outlook.com>
2023-04-30 16:39:28 +0200

    create add acount

--- src/main/java/bankapp/dao/AccountDao.java
+++ src/main/java/bankapp/dao/AccountDao.java
@@ -1,5 +1,46 @@
 package bankapp.dao;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import bankapp.model.Account;
+
 public class AccountDao {
 
+	
+	public void addAcount(Account cc) {
+
+		ArrayList<Account> tab = new ArrayList<>();
+
+		try {
+
+			FileInputStream io = new FileInputStream("C:\\Users\\Jekyll\\Documents\\textJsp\\Account.txt");
+			ObjectInputStream o = new ObjectInputStream(io); 
+
+			tab = (ArrayList<Account>) o.readObject(); 
+			tab.add(cc);
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace(); 
+
+		}
+
+		
+		try {
+
+			
+			FileOutputStream io = new FileOutputStream("C:\\Users\\Jekyll\\Documents\\textJsp\\Account.txt");
+			ObjectOutputStream o = new ObjectOutputStream(io); 
+			
+			o.writeObject(tab); 
+
+		} catch (IOException e) {
+			e.printStackTrace(); 
+
+		}
+	}
 }
