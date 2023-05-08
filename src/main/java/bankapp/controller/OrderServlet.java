6835324e108aa9e10e775d68ef5644e76272d263
Melcn <melaniearinosy@outlook.com>
2023-05-08 14:39:45 +0200

    doPost + doGet in OrderServlet

--- src/main/java/bankapp/controller/OrderServlet.java
+++ src/main/java/bankapp/controller/OrderServlet.java
@@ -1,38 +1,60 @@
 package bankapp.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class OrderServlet
- */
+import bankapp.dao.OrderDao;
+import bankapp.model.Order;
+
+
+@WebServlet("/OrderServlet")
 public class OrderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public OrderServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		
+		
+		OrderDao orderDao = new OrderDao();
+		request.setAttribute("commande", orderDao.showAllOrders());
+		PrintWriter p = response.getWriter();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+		p.print(orderDao.showAllOrders().toString());
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+
+		String date = request.getParameter("date");
+		String amount = request.getParameter("amount");
+		String payment = request.getParameter("payment");
+		String payStatus = request.getParameter("paymentStatus");
+		String delivery = request.getParameter("delivery");
+		String deliveryStatus = request.getParameter("deliveryStatus");
+		
+		Order order = new Order();
+		
+		order.setOrderDate(date);
+		order.setOrderAmount(amount);
+		order.setDeliveryMethod(delivery);
+		order.setPaymentStatus(payStatus);
+		order.setDeliveryStatus(deliveryStatus);
+		
+		OrderDao orderDao = new OrderDao();
+		orderDao.addOrder(order);
+		
+		ArrayList<Order> arrayCoco = OrderDao.showAllOrders();
+		
+		request.setAttribute("commande", order);
+		request.setAttribute("array", arrayCoco);
+		
+
+		this.getServletContext().getRequestDispatcher("/WEB-INF/OrderView.jsp").forward(request, response);
+	
 	}
 
 }
