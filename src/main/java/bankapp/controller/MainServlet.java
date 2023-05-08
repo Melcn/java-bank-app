0174f55851d8992ed2a5c6004efaa57af760f824
Melcn <melaniearinosy@outlook.com>
2023-05-08 14:44:15 +0200

    doGet in mainServlet

--- src/main/java/bankapp/controller/MainServlet.java
+++ src/main/java/bankapp/controller/MainServlet.java
@@ -1,35 +1,38 @@
 package bankapp.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MainServlet
- */
+import bankapp.dao.CustomerDao;
+
+
+
+@WebServlet("/mappage")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+    
     public MainServlet() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		CustomerDao dao = new CustomerDao();
+		request.setAttribute("account", dao.showAllAccount());
+		PrintWriter p = response.getWriter();
+
+		p.print(dao.showAllAccount().toString());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
