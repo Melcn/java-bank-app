164dd2abf67d1e9ff5ebf702e4f49ff028c4b7c7
Melcn <melaniearinosy@outlook.com>
2023-05-08 14:49:19 +0200

    doPost in MainServlet

--- src/main/java/bankapp/controller/MainServlet.java
+++ src/main/java/bankapp/controller/MainServlet.java
@@ -2,13 +2,17 @@ package bankapp.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.ClicliDAO;
+import Model.Clicli;
 import bankapp.dao.CustomerDao;
+import bankapp.model.Customer;
 
 
 
@@ -33,9 +37,36 @@ public class MainServlet extends HttpServlet {
 	}
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		// doGet(request, response);
+
+
+		String name = request.getParameter("name");
+		String firstName = request.getParameter("firstName");
+		String adress = request.getParameter("adress");
+		String tel = request.getParameter("tel");
+		String email = request.getParameter("email");
+		
+		Customer customer = new Customer();
+		customer.setCustomerName(name);
+		customer.setCustomerFirstName(firstName);
+		customer.setCustomerAdress(adress);
+		customer.setCustomerPhone(tel);
+		customer.setCustomerMail(email);
+		
+		CustomerDao customerDao = new CustomerDao();
+		customerDao.addAccount(customer);
+		ArrayList<Customer> arrayCustomer = customerDao.showAllAccount();
+		
+		
+
+		request.setAttribute("account", customer);
+		request.setAttribute("array", arrayCustomer);
+		
+
+		this.getServletContext().getRequestDispatcher("/WEB-INF/view.jsp").forward(request, response);
 	}
 
 }
